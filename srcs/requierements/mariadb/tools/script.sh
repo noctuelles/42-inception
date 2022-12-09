85fdc7ea0ebfea5198bb98eb46c4f6e0d3ac1ab5
noctuelles <plouvel@student.42.fr>
2022-12-09 22:59:26 +0100

    finished mariadb script

--- srcs/requierements/mariadb/tools/script.sh
+++ srcs/requierements/mariadb/tools/script.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
-echo "-- MYSQL Secure Installation --"
+echo -n "Initializing MariaDB data directory..."
 
-echo -n "Updating root password"
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root'"
-if [ $? -eq 0]; then
-	echo "... Success!"
-else
-	echo "... Failed!"
+if [[ ! -d /run/mysqld ]]; then
+	mkdir -p /run/mysqld/
+	chown -R mysql:mysql /run/mysqld/
+fi
 
-echo -n "Removing anonymous users"
-mysql -e "DELETE FROM mysql.user WHERE User='';"
-if [ $? -eq 0]; then
-	echo "... Success!"
-else
-	echo "... Failed!"
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	
+	chown -R mysql:mysql /var/lib/mysql
 
-echo -n "Removing remote root"
-mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-if [ $? -eq 0]; then
-	echo "... Success!"
-else
-	echo "... Failed!"
+	if mysql_install_db --user=mysql \
+		--datadir=/var/lib/mysql \
+		> /dev/null; then
+		echo " Sucess!"
+	else
+		echo " Failure!"
+		exit 1
+	fi
 
-echo -n "Removing test database"
-mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
-if [ $? -eq 0]; then
-	echo "... Success!"
-else
-	echo "... Failed!"
+	echo "Executing init SQL script..."
 
-echo -n "Reloading privilege tables"
-mysql -e "FLUSH PRIVILEGES"
-if [ $? -eq 0]; then
-	echo "... Success!"
+	tmpfile=$(mktemp)
+
+	cat << EOF > $tmpfile
+UPDATE mysql.user SET Password = PASSWORD('${MYSQL_ROOT_PASSWORD}') WHERE User = 'root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE ${WP_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${WP_DATABASE_USER}'@'localhost' IDENTIFIED BY '${WP_DATABASE_USER_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${WP_DATABASE}.* TO '${WP_DATABASE_USER}'@'localhost' IDENTIFIED BY '${WP_DATABASE_USER_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+	# https://mariadb.com/kb/en/mysqld-options/#-bootstrap
+
+	if ! mysqld --user=mysql --verbose --bootstrap < $tmpfile ; then
+		exit 1
+	fi
+
+	rm -rf $tmpfile
 else
-	echo "... Failed!"
+	echo " already configured."
+fi
+
+echo "All good now! Starting deamon."
+
+exec mysqld --user=mysql
