3b4fc8d9e5e8e8551dc37cf8fb751b32b0f2aa03
noctuelles <plouvel@student.42.fr>
2022-12-11 22:43:33 +0100

    adminer update

--- srcs/requierements/adminer/tools/configure.sh
+++ srcs/requierements/adminer/tools/configure.sh
@@ -2,4 +2,11 @@
 
 ./wait
 
-curl ${ADM_MYSQL_DOWNLOAD_LINK} -o
+#if [ ! -d ${ADM_PATH} ]; then
+#	mkdir -p ${ADM_PATH}
+#	curl ${ADM_MYSQL_DOWNLOAD_LINK} -o ${ADM_PATH}/index.php
+#fi
+
+echo "Starting php-fpm..."
+
+exec php-fpm8.2 --nodaemonize
--- srcs/requierements/nginx/tools/configure.sh
+++ srcs/requierements/nginx/tools/configure.sh
@@ -4,7 +4,8 @@
 
 export DOLLAR="$"
 
-envsubst < plouvel.42.fr.conf.template > $NGINX_CONFIG_DIR/default
+envsubst < plouvel.42.fr.conf.template > $NGINX_CONFIG_DIR/plouvel.42.fr.conf
+envsubst < adminer.conf.template > $NGINX_CONFIG_DIR/adminer.conf
 
 echo "Starting nginx..."
 exec nginx -g 'daemon off;'
