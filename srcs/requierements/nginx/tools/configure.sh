2791abc35e95aac2c729d640a2c55ed28c63a40c
noctuelles <plouvel@student.42.fr>
2022-12-12 06:34:19 +0100

    added perso website

--- srcs/requierements/nginx/tools/configure.sh
+++ srcs/requierements/nginx/tools/configure.sh
@@ -6,6 +6,7 @@ export DOLLAR="$"
 
 envsubst < plouvel.42.fr.conf.template > $NGINX_CONFIG_DIR/plouvel.42.fr.conf
 envsubst < adminer.42.fr.conf.template > $NGINX_CONFIG_DIR/adminer.42.fr.conf
+envsubst < perso.42.fr.conf.template > $NGINX_CONFIG_DIR/perso.42.fr.conf
 
 echo "Starting nginx..."
 exec nginx -g 'daemon off;'
